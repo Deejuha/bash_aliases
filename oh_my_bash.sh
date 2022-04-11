1afcc17dcddbb556a0aae37b9e531dcc126fcbc7
Michał Juszczyk <31712401+Deejuha@users.noreply.github.com>
2022-04-11 21:13:03 +0200

    Update oh_my_bash.sh

--- oh_my_bash.sh
+++ oh_my_bash.sh
@@ -1,4 +1,4 @@
-git clone git://github.com/ohmybash/oh-my-bash.git ~/.oh-my-bash
+git clone https://github.com/ohmybash/oh-my-bash.git ~/.oh-my-bash
 cp ~/.bashrc ~/.bashrc.orig
 cp ~/bash_aliases/.bashrc ~/.bashrc
 sh ~/bash_aliases/set_git_aliases.sh
